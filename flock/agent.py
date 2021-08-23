0f72b1ea5877c55cd091c2fdd5e5c32411cf0d66
ClaireGuerin <guerin.claire01@gmail.com>
2021-08-23 12:19:20 +0200

    alignement force calculated. need to calculate agent new position accordingly

--- flock/agent.py
+++ flock/agent.py
@@ -1,6 +1,6 @@
 from mesa import Agent
 import logging
-#import numpy as np
+import numpy as np
 import math as m
 from flock.logging import *
 
@@ -9,22 +9,37 @@ class Fish(Agent):
     
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-
-        self.bodyLength = 3 # body length in arbitrary unit
-        self.speed = 2 # speed in body length / s
-        self.direction = 2 * m.pi / 3 # direction angle in radians
+        self.heading = (0, 0) # heading vector of agent, zero before the agent is placed in space
 
     def head(self):
-        newX = self.pos[0] + self.speed * m.cos(self.direction)
-        newY = self.pos[1] + self.speed * m.sin(self.direction)
+        newX = self.pos[0] + self.model.parameters.cruiseSpeed * m.cos(self.direction)
+        newY = self.pos[1] + self.model.parameters.cruiseSpeed * m.sin(self.direction)
         self.newPos = (newX, newY)
 
+    def group(self. radius):
+         # pos: FloatCoordinate, radius: float, include_center: bool = True
+        self.model.space.get_neighbors(self.pos, radius, False)
         
     def step(self):
         """ The agent's step will go here.
-        For demonstration purposes, we increase body length"""
-        self.bodyLength += self.unique_id
-        #self.n_neighbors = len( self.model.space.get_neighbors(self.pos, 1, False) ) # pos: FloatCoordinate, radius: float, include_center: bool = True
+        The agent has three areas:
+        - cohesion (radius 2)
+        - alignment (radius 5)
+        - separation (radius 15) """
+
+        # ALIGNMENT
+        alignmentGroup = self.group(self.model.parameters.alignmentRadius)
+        xSum, ySum = 0
+
+        for neighbor in alignmentGroup:
+            xSum += neighbor.pos[0]
+            ySum += neighbor.pos[1]
+
+        alignmentDirection = -1 / len(alignmentGroup) * np.array([xSum, ySum])
+        alignmentMagnitude = m.sqrt( np.sum( np.square(alignmentDirection - self.heading) ) )
+
+        self.alignmentForce = self.model.parameters.alignmentWeight * alignmentDirection / alignmentMagnitude
+
         self.head()
                
     def advance(self):
