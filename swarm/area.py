14adfc450939f606b464be16cf39a3e894554db9
ClaireGuerin <guerin.claire01@gmail.com>
2021-09-01 17:14:01 +0200

    calculating the necessary angles for checking which neighbour is in vs out the blind spot

--- swarm/area.py
+++ swarm/area.py
@@ -1,31 +1,61 @@
 from mesa.space import *
+from swarm.vectors import *
+from math import radians, atan2
 
 class Area(ContinuousSpace):
 	""" Child class of Mesa Subclass ContinuousSpace from Mesa Space
 	    Overwrites get_neighbors method to include blind spot(s) """
 	def get_neighbors(
-		self, pos: FloatCoordinate, radius: float, include_center: bool = True
+		self, 
+		pos: FloatCoordinate, 
+		radius: float, 
+		include_center: bool = True,
+		focal_heading: list,
+		blind_angle: float,
+		front: bool = False
 	) -> List[GridContent]:
 		"""Get all objects within a certain radius.
 		Args:
 			pos: (x,y) coordinate tuple to center the search at.
 			radius: Get all the objects within this distance of the center.
-			blind_angle: Angle of the blind spot
-			front: If True, include blind spot in front. Else, blind spot will only be in the back
 			include_center: If True, include an object at the *exact* provided
 							coordinates. i.e. if you are searching for the
 							neighbors of a given agent, True will include that
 							agent in the results.
+			focal_heading: focal.heading vector
+			blind_angle: Angle of the blind spot, in degrees (will be translated to radians)
+			front: If True, include blind spot in front. Else, blind spot will only be in the back
 		"""
-		deltas = np.abs(self._agent_points - np.array(pos))
+		# Neighbors are agents: 
+		# 1) within radius, i.e. distance between pos and agent.pos <= radius
+		deltas = np.abs( self._agent_points - np.array(pos) )
 
 		if self.torus:
 			deltas = np.minimum(deltas, self.size - deltas)
 		dists = deltas[:, 0] ** 2 + deltas[:, 1] ** 2
 
 		(idxs,) = np.where(dists <= radius ** 2)
-		neighbors = [
+
+
+		# 2) outside of blind spot (in the back, and in the front if True), i.e. 
+		# IN THE BACK:
+		# !(beta + Pi - alpha / 2 < gamma < beta + Pi + alpha / 2), where
+		# alpha = blind_angle in radians
+		# beta = heading angle of focal agent
+		# gamma = angle of agent.pos from x-axis in radians
+
+		alpha = radians(blind_angle)
+		beta = angle(focal_heading) 
+		gamma = np.atan2( self._agent_points - np.array(pos) ) # WARNING: 
+		# returns value between -Pi and Pi, 
+		# should check whether this is automatically translated
+
+
+		# Get neighbors
+
+		neighbors_no_blind_spot = [
 			self._index_to_agent[x] for x in idxs if include_center or dists[x] > 0
 		]
+		
 
 		return neighbors
--- swarm/vectors.py
+++ swarm/vectors.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 def magnitude(vec):
+	""" Returns the magnitude of vector vec """
 	return np.sqrt( vec.dot(vec) )
 
 def unit(vec):
+	""" Returns the unit vector of vec"""
 	mag = magnitude(vec)
 	if mag == 0.0:
 		return 0.0
@@ -11,7 +13,14 @@ def unit(vec):
 		return vec / magnitude(vec)
 
 def direction(vec, n):
+	""" Returns the direction vector """
 	return - 1 / n * vec
 
 def force(weight, sumVec, n):
+	""" Returns the force vector """
 	return weight * unit( direction(sumVec, n) )
+
+def angle(vec):
+	""" Returns the angle in radians of a vector vec with the x-axis +x
+	    θ = cos^−1(a_x/|a⃗ |) """
+	return np.arccos(vec[0] / magnitude(vec))
